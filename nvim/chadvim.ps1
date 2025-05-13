4ecd1b247067e5b2d356f5fcb77ffa4051c045b8
MASTERJUDAH <141491462+JoshuaThadi@users.noreply.github.com>
2025-05-13 16:36:04 +0530

    Delete nvim directory

--- nvim/chadvim.ps1
+++
@@ -1,8 +0,0 @@
-# Remove any existing nvim config
-rm -rf ~/.config/nvim ~/.local/share/nvim ~/.cache/nvim
-
-# Clone ChadVim
-git clone https://github.com/chadcat7/chadVim ~/.config/nvim
-
-# Run the install script
-nvim
